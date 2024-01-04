d146fa9e68aaa6c6fd56c2183f600b8d3aadc49b
Jhojan <jhojanrock98@gmail.com>
2024-01-04 10:44:51 -0500

    Primera parte del diseño del login

--- lib/src/presentation/pages/auth/login/login_page.dart
+++ lib/src/presentation/pages/auth/login/login_page.dart
@@ -5,9 +5,56 @@ class LoginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Login Page'),
+    return  Scaffold(
+      body: Container(
+        width: double.infinity,
+        color: Colors.amber,
+        child:  Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const Icon(
+              Icons.account_circle,
+              size: 100,
+              color: Colors.indigo,
+            ),
+            const Text(
+              'Login Page',
+              style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+                color: Colors.indigo,
+              ),
+            ),
+           const TextField(
+              decoration: InputDecoration(
+                hintText: 'Email',
+                border: OutlineInputBorder(),
+              ),
+            ),
+           const TextField(
+              decoration: InputDecoration(
+                hintText: 'Contraseña',
+                border: OutlineInputBorder(),
+              ),
+            ),
+          ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.green,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20),
+                ),
+              ),
+              child: const Text('INICIAR SESIÓN')
+            ),
+          const Text("¿No tienes cuenta?"),
+          TextButton(
+              onPressed: () {},
+              child: const Text('REGISTRATE'),
+            ),
+          ],
+        ),
       )
     );
   }
